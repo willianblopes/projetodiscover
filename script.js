47ffa2a76246a4b1825af6a2c18f5220db484e12
Willian Barreto <willianblopes@gmail.com>
2023-12-03 23:43:00 -0300

    arrumar linha

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
-  
-  if (html.classList.contains('light')) {
-    img.setAttribute('src', './assets/assets/avatar-light.png')
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
   } else {
-   img.setAttribute("src", "../assets/assets/avatar.png")
+    img.setAttribute("src", "../assets/assets/avatar.png")
   }
-    if (html.classList.contains('light')) {
-      img.setAttribute('alt', "Willian Barreto1")
-    } else {
-      img.setAttribute('alt', "Willian")
-    }
+ /* if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Willian Barreto1")
+  } else {
+    img.setAttribute("alt", "Willian")
+  }*/
 }
